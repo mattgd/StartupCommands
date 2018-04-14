b01037e1864276bc328a3ee6e1a24c3e4210a9a8
mattgd <mattdzwonczyk@gmail.com>
2018-04-14 17:01:24 -0400

    Add additional test for StartupCommands.isInteger().

---
+++ test/me/mattgd/startupcommands/StartupCommandsTest.java
@@ -0,0 +1,26 @@
+package me.mattgd.startupcommands;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StartupCommandsTest {
+
+    /**
+     * Tests the {@link me.mattgd.startupcommands.StartupCommands#isInteger(String)} method.
+     */
+    @Test
+    public void isInteger() {
+        String notInt = "not an int";
+        assertFalse(StartupCommands.isInteger(notInt));
+
+        String anInt = "1452";
+        assertTrue(StartupCommands.isInteger(anInt));
+
+        String doubleNotInt = "15.23";
+        assertFalse(StartupCommands.isInteger(doubleNotInt));
+
+        String notQuiteInt = "15.00";
+        assertFalse(StartupCommands.isInteger(notQuiteInt));
+    }
+}
